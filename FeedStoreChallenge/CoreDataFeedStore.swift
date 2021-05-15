76060b0aefe1126fbbdabcb4138a0124130fc86c
oligazar <nikelodian@gmail.com>
2021-05-15 14:08:05 +0300

    Create ManagedCache and ManagedFeedImage classes as managed model representations

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -40,3 +40,18 @@ public final class CoreDataFeedStore: FeedStore {
 		fatalError("Must be implemented")
 	}
 }
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+	@NSManaged var timestamp: Date
+	@NSManaged var feed: NSOrderedSet
+}
+
+@objc(ManagedFeedImage)
+class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
+}
