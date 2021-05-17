cd1a516d65a042682f3739f71897c87ccddc0e06
oligazar <nikelodian@gmail.com>
2021-05-17 07:59:16 +0300

    `ManagedFeedImage` extracted to it's own file

--- FeedStoreChallenge/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ FeedStoreChallenge/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -80,27 +80,3 @@ public final class CoreDataFeedStore: FeedStore {
 		}
 	}
 }
-
-@objc(ManagedFeedImage)
-class ManagedFeedImage: NSManagedObject {
-	@NSManaged var id: UUID
-	@NSManaged var imageDescription: String?
-	@NSManaged var location: String?
-	@NSManaged var url: URL
-	@NSManaged var cache: ManagedCache
-
-	var local: LocalFeedImage {
-		return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
-	}
-
-	static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-		return NSOrderedSet(array: feed.map { local in
-			let managed = ManagedFeedImage(context: context)
-			managed.id = local.id
-			managed.imageDescription = local.description
-			managed.location = local.location
-			managed.url = local.url
-			return managed
-		})
-	}
-}
---
+++ FeedStoreChallenge/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,33 @@
+//
+//  ManagedFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Alexander on 17/5/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
+
+	internal var local: LocalFeedImage {
+		return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+	}
+
+	internal static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+		return NSOrderedSet(array: feed.map { local in
+			let managed = ManagedFeedImage(context: context)
+			managed.id = local.id
+			managed.imageDescription = local.description
+			managed.location = local.location
+			managed.url = local.url
+			return managed
+		})
+	}
+}
